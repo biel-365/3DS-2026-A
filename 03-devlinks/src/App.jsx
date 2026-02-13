c0aae4bd1b2c5d5f962713c3b6fd819701368c16
biel-365 <aroiogabriel@gmail.com>
2026-02-13 16:57:00 -0300

    feat: adicionar funcionalidade de tema claro/escuro e refatorar componentes para melhor usabilidade

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import './App.css'
 import Botoes from './components/Botoes/Botoes'
 import Footer from './components/Footer/Footer'
@@ -7,20 +8,29 @@ import Switch from './components/Switch/Switch'
 
 function App() {
 
+  const[IsLight,setIsLight] = useState(true)
+
+  const troca = () => {
+    setIsLight(!IsLight);
+  };
+
   return (
-    <div id='App'>
+
+    <div id='App' className={IsLight ? "light" : ""}>
 
     <Perfil fotoPerfil={"https://placehold.co/200"}>Gabriel Lopes Aroio</Perfil>
 
-   <Switch />
-   
 
+   <Switch troca={troca} isLight={IsLight} />
+
+    <div id="Botoes">
     <ul>
      <Botoes url={""}>Inscreva-se</Botoes>
      <Botoes url={""}>Minha Playlist</Botoes>
      <Botoes url={""}>Me Pague um Café!</Botoes>
      <Botoes url={""}>Saiba Mais do curso DEV</Botoes> 
     </ul>
+    </div>
 
     <div id="RedesSociais">
       <RedesSociais url={"https://github.com"} icon={"logo-github"}></RedesSociais>
