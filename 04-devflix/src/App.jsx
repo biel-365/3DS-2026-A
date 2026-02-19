d61cb4ebb390ab9e4d1c1a4eb7564b7223305ffa
biel-365 <aroiogabriel@gmail.com>
2026-02-19 11:19:31 -0300

    feat: implementar componente Footer e estilização, adicionando logo e campo de pesquisa ao App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,10 +1,31 @@
 import "./App.css";
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Footer from "./components/Footer/Footer";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        className="logo"
+        src={logo}
+        alt="Logo da Netflix com fundo preto e destaque vermelho para a palavra DEVFLIX, enfatizando a marca de streaming de filmes e séries."
+      ></img>
 
-export default App
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Insira o nome de um filme ou uma série..."
+          img
+          role="button"
+          src={lupa}
+          alt="botão de ação para pesquisa!"
+        />
+      </div>
 
+      <Footer>Gabriel Lopes Aroio</Footer>
+    </div>
+  );
+};
+
+export default App;
---
+++ 04-devflix/src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React, { Children } from "react";
+import "./Footer.module.css";
+
+const Footer = ({ children }) => {
+  return (
+    <footer>
+      <p>
+        Feito com 💖 por <a href="https://github.com">{children}</a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
