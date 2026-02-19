e5bd9f9b2df9164771f2f39dc893e7efeb3a7b58
biel-365 <aroiogabriel@gmail.com>
2026-02-19 10:43:59 -0300

    feat: adequando codigo fonte do index.html para pt-BR troa de icone e titulo, adicionando imagens para trabalhar no projeto, removendo imagens iniciais do projeto VITE e outras coisas

--- 04-devflix/vite.config.js
+++ 04-devflix/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Muda a porta para o padrão vite (3000)
+    open: true, // Abre o navegador na hora direto
+    host: true, // Permite que tenha acesso via IP na rede local 
+  }
 })
