34f4602e9204f3566bfb594ea73be7187c006070
biel-365 <aroiogabriel@gmail.com>
2026-04-24 08:02:56 -0300

    Adição de componente de chat com gerenciamento e estilização de mensagens
    - Implementação do componente Chat para gerenciar o envio e recebimento de mensagens via socket.
    
    - Adição de um campo de entrada para digitação de mensagens e um botão de envio utilizando Material-UI.
    
    - Criação do arquivo Chat.module.css para estilizar a interface do chat, incluindo os contêineres de mensagens e o comportamento de rolagem.
    
    - Integração da rolagem automática para a mensagem mais recente.
    
    - Implementação da limpeza dos listeners de socket para evitar vazamentos de memória.

--- 06-devChat/client/src/App.jsx
+++ 06-devChat/client/src/App.jsx
@@ -1,12 +1,21 @@
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
+
+import Chat from "./components/Chat/Chat";
 import Join from "./components/Join/Join";
 
 function App() {
+  const [chatVisibility, setChatVisibility] = useState(false);
+  const [socket, setSocket] = useState(null);
   return (
     <>
-    <Join />
+      {chatVisibility ? (
+        <Chat socket={socket} />
+      ) : (
+        <Join setSocket={setSocket} setChatVisibility={setChatVisibility} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
